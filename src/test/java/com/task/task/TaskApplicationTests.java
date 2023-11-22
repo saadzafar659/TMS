7b59d143f936d3de74028cb263eb1892a83b2863
saadzafar659 <saadzafar659@gmail.com>
2023-11-22 21:37:59 +0100

    First Push

---
+++ src/test/java/com/task/task/TaskApplicationTests.java
@@ -0,0 +1,14 @@
+package com.task.task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.Assertions;
+
+@SpringBootTest
+class TaskApplicationTests {
+    @Test
+    void contextLoads() {
+        // Add your assertion here
+        Assertions.assertTrue(true, "The context should load successfully.");
+    }
+}
